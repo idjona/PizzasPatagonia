0fefff48f499fc66e00f60a780792777deb7468c
Jonathan <freddyreql1>
2021-07-12 21:56:51 -0400

    Initial commit

---
+++ app/src/main/java/com/jona/pizzaspatagonia/SeleccionPizzaActivity.java
@@ -0,0 +1,14 @@
+package com.jona.pizzaspatagonia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionPizzaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_pizza);
+    }
+}
